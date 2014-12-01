b0321720bd4a6e2b29199e0f9bcba58df72944a2
vikmenon <vikmenon@ufl.edu>
2014-11-30 21:56:28 -0500

    Vikram Menon/vikmenon on behalf of Allen Stark
    Allen's implementation of a Desktop Sincre Client and Video Player.

---
+++ Code/Allen/SincreDesktopClient/Sincre_deskapp_v2_working/SincreDisplay_alone.java
@@ -0,0 +1,147 @@
+package sincre.dapp.com;
+
+
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.BrowserFactory;
+import com.teamdev.jxbrowser.chromium.events.FinishLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.LoadAdapter;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+@SuppressWarnings("serial")
+class SincreDisplay_alone extends JFrame {
+
+	
+	
+	static JTextField text = new JTextField("...{Movie URL}...") ;
+	static JFrame frame = new JFrame();
+	public static Object DISPOSE_ON_CLOSE;
+
+	public static void main(String[] args){
+        final Browser browser = BrowserFactory.create();
+
+        
+        browser.addLoadListener(new LoadAdapter() {
+            @Override
+            public void onFinishLoadingFrame(FinishLoadingEvent event) {
+                if (event.isMainFrame()) {
+       
+                    System.out.println("URL = " + browser.getURL());
+                }
+            }
+        });
+        
+       
+        
+        JButton movie1 = new JButton("1st Match");
+        movie1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	browser.loadURL("https://s3.amazonaws.com/starkeallen/Jurassic.mp4");
+            	text.setText(browser.getURL());
+            }
+        });
+
+        JButton movie2 = new JButton("2nd Match");
+        movie2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	browser.loadURL("https://s3.amazonaws.com/starkeallen/Purge.mp4");
+            	text.setText(browser.getURL());
+            }
+        });
+        
+        JButton movie3 = new JButton("3rd Match");
+        movie3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	browser.loadURL("https://s3.amazonaws.com/starkeallen/sample.mp4");
+            	text.setText(browser.getURL());
+            }
+        });
+        
+        JButton movie4 = new JButton("4th Match");
+        movie4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+				browser.loadURL("https://s3.amazonaws.com/starkeallen/Dumb.mp4");
+				text.setText(browser.getURL());
+            }
+        });
+        
+        JButton movie5 = new JButton("5th Match");
+        movie5.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+				browser.loadURL("https://s3.amazonaws.com/starkeallen/Dumb.mp4");
+				text.setText(browser.getURL());
+            }
+        });
+        
+        JButton movie6 = new JButton("6th Match");
+        movie6.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+				browser.loadURL("https://s3.amazonaws.com/starkeallen/Dumb.mp4");
+				text.setText(browser.getURL());
+            }
+        });
+        
+        JButton movie7 = new JButton("7th Match");
+        movie7.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	text.setText(browser.getURL());
+            }
+        });
+        
+        JButton movie8 = new JButton("8th Match");
+        movie8.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+				browser.loadURL("https://s3.amazonaws.com/starkeallen/Dumb.mp4");
+				text.setText(browser.getURL());
+            }
+        });
+        
+        JButton movie9 = new JButton("9th Match");
+        movie9.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+				browser.loadURL("https://s3.amazonaws.com/starkeallen/Dumb.mp4");
+				text.setText(browser.getURL());
+            }
+        });
+        
+        JButton movie0 = new JButton("10th Match");
+        movie0.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+				browser.loadURL("https://s3.amazonaws.com/starkeallen/Dumb.mp4");
+				text.setText(browser.getURL());
+            }
+        });
+
+        
+        
+        JPanel toolBar = new JPanel();
+        toolBar.add(movie1);
+        toolBar.add(movie2);
+        toolBar.add(movie3);
+        toolBar.add(movie4);
+        toolBar.add(movie5);
+        toolBar.add(movie6);
+        toolBar.add(movie7);
+        toolBar.add(movie8);
+        toolBar.add(movie9);
+        toolBar.add(movie0);
+        toolBar.add(text);
+
+        
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(browser.getView().getComponent(), BorderLayout.CENTER);
+        frame.add(toolBar, BorderLayout.SOUTH);
+        frame.add(text, BorderLayout.PAGE_START);
+        frame.add(new SincreFileChooser_alone(), BorderLayout.WEST);
+        frame.setSize(1500, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+        
+    }
---
+++ Code/Allen/SincreDesktopClient/Sincre_deskapp_v2_working/SincreFileChooser_alone.java
@@ -0,0 +1,129 @@
+
+/*
+ * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
+ * Allen Starke - The above copyright stated that even though this information was modified to fit team Sincre 
+ * need that the above statement should still be included. And it allows for redistribution
+ *
+ */
+
+package sincre.dapp.com;
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+import javax.swing.filechooser.*;
+
+
+
+public class SincreFileChooser_alone extends JPanel implements ActionListener {
+    
+	private static final long serialVersionUID = 1L;
+	static private final String newline = "\n";
+    JButton browseButton, uploadButton;
+    JTextArea log;
+    JFileChooser fc;
+    static JFrame frame = new JFrame("FileChooser");
+   
+
+    public SincreFileChooser_alone() {
+        super(new BorderLayout());
+
+        //Create the log
+        log = new JTextArea(3,20);
+        log.setMargin(new Insets(5,5,5,5));
+        log.setEditable(false);
+        JScrollPane logScrollPane = new JScrollPane(log);
+
+        //Create a file chooser
+        fc = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(
+                "mp4/png Files", "mp4","png");
+        fc.setFileFilter(filter);
+
+     
+        //fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+       
+
+      
+        browseButton = new JButton("Browse a File...",createImageIcon("Open16.gif"));
+        browseButton.addActionListener(this);
+
+        
+        uploadButton = new JButton("Upload a File...",createImageIcon("Save16.gif"));
+        uploadButton.addActionListener(this);
+
+        
+        JPanel buttonPanel = new JPanel(); 
+        buttonPanel.add(browseButton);
+        buttonPanel.add(uploadButton);
+
+        
+        add(buttonPanel, BorderLayout.CENTER);
+        add(logScrollPane, BorderLayout.PAGE_START);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        //Handle browse button action.
+        if (e.getSource() == browseButton) {
+            int returnVal = fc.showOpenDialog(SincreFileChooser_alone.this);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                
+                log.append("Uploading: " + file.getName() + "." + newline + "File Path:" + file.getPath());
+            } else {
+                log.append("Browse command cancelled by user." + newline);
+            }
+            log.setCaretPosition(log.getDocument().getLength());
+
+      
+        } else if (e.getSource() == uploadButton) {
+        	
+        	
+        }
+    }
+
+    
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = SincreFileChooser_alone.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    
+    private static void GUI() {
+        //Create and set up the window.
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add content to the window.
+        frame.add(new SincreFileChooser_alone(), BorderLayout.SOUTH);
+        
+
+        //Display the window.
+        frame.pack();
+        frame.setSize(800, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        
+        SwingUtilities.invokeLater(new Runnable() {
+        	
+            public void run() {
+            	
+                
+                UIManager.put("swing.boldMetal", Boolean.FALSE); 
+                GUI();
+            }
+        });
+    }
+}
